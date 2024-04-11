5386f32525d7e53127d1238c6bec40bfa88f4245
Jim Hargrave <jim.hargrave@strakergroup.com>
2024-04-11 11:11:38 -0600

    add new depdenies for pixi and make an FHS env

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
-  description = "An FHS shell with Python and CUDA.";
+  description = "An FHS shell with Python and CUDA using Pixi.";
 
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -14,41 +14,65 @@
         config.cudannSupport = true;
         config.nvidiaSupport = true;
       };
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs;
-          [
-            cudaPackages_12_3.cudatoolkit
-            cudaPackages_12_3.libcublas
-            cudaPackages_12_3.cudnn
-            linuxPackages.nvidia_x11_stable_open
-            libGLU
-            libGL
-            xorg.libXi
-            xorg.libXmu
-            freeglut
-            xorg.libXext
-            xorg.libX11
-            xorg.libXv
-            xorg.libXrandr
-            zlib
-            ncurses5
-            stdenv.cc
-            binutils
-            ffmpeg
-            pixi
-            python311
-            python311Packages.pip
-            python311Packages.virtualenv
-          ];
-        shellHook = ''
-          # cuda
-          export CUDA_PATH=${pkgs.cudaPackages_12_3.cudatoolkit}
+      fhs = pkgs.buildFHSUserEnv {
+        name = "python-pixi-shell";
+        targetPkgs = pkgs: with pkgs; [
+          cudaPackages_12.cudatoolkit
+          cudaPackages_12.libcublas
+          cudaPackages_12.cudnn
+          cudaPackages_12.cuda_nvcc
+          #cudaPackages_12.tensorrt
+          cudaPackages_12.cutensor
+          linuxPackages.nvidia_x11_stable_open
+          libGLU
+          libGL
+          xorg.libICE
+          xorg.libSM
+          xorg.libX11
+          xorg.libXext
+          xorg.libXi
+          xorg.libXmu
+          xorg.libXrandr
+          xorg.libXrender
+          xorg.libXv
+          freeglut
+          zlib
+          ncurses5
+          stdenv.cc
+          binutils
+          ffmpeg
+          autoconf
+          curl
+          freeglut
+          gcc
+          git
+          gitRepo
+          gnumake
+          gnupg
+          gperf
+          libselinux
+          m4
+          procps
+          unzip
+          util-linux
+          wget
+
+          pixi
+          python311
+          python311Packages.pip
+          python311Packages.virtualenv
+        ];
+        profile = ''
+          # CUDA
+          export CUDA_PATH=${pkgs.cudaPackages_12.cudatoolkit}
           export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11_stable_open}/lib
           export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11_stable_open}/lib"
           export EXTRA_CCFLAGS="-I/usr/include"
         '';
+        runScript = "bash";
       };
+    in
+    {
+      devShells.${system}.default = fhs.env;
     };
 }
