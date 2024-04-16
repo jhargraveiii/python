c0e4063188f225b6b7de883318112bd2d28eae66
Jim Hargrave <jim.hargrave@strakergroup.com>
2024-04-15 19:56:17 -0600

    attempt to solve jax and cuda depdencies - no luck

--- flake.nix
+++ flake.nix
@@ -3,25 +3,25 @@
 
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
+
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
         config.allowBroken = true;
-        config.cudaSupport = true;
-        config.cudannSupport = true;
         config.nvidiaSupport = true;
       };
+
+      latest_pixi = pkgs.callPackage ./pixi.nix {
+        inherit (pkgs) stdenv rustPlatform fetchFromGitHub pkg-config libgit2 openssl installShellFiles;
+      };
+
       fhs = pkgs.buildFHSUserEnv {
         name = "cuda";
         targetPkgs = pkgs: with pkgs; [
-          cudaPackages_12_3.cudatoolkit
-          cudaPackages_12_3.libcublas
-          cudaPackages_12_3.cudnn
-          cudaPackages_12_3.cuda_nvcc
-          cudaPackages_12_3.cutensor
+          latest_pixi
           linuxPackages.nvidia_x11_stable_open
           libGLU
           libGL
@@ -56,25 +56,15 @@
           unzip
           util-linux
           wget
-          pixi
-          bazel
         ];
         profile = ''
-          # CUDA
-          export CUDA_PATH=${pkgs.cudaPackages_12_3.cudatoolkit}
+          export CUDA_PATH=/home/jimh/DATA2/python/.pixi/envs/default
           export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11_stable_open}/lib
           export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11_stable_open}/lib"
           export EXTRA_CCFLAGS="-I/usr/include"
           export KERAS_BACKEND="jax"
           export JAX_PLATFORM_NAME="cuda"
         '';
-
-        runScript = ''
-          #!/bin/sh
-
-          pixi install
-          pixi shell
-        ''; 
       };
     in
     {
