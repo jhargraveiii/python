85d7cc92a9c8de1e8a40ffb39bce0a7bf54a956d
Jim Hargrave <jim.hargrave@strakergroup.com>
2024-04-16 11:27:19 -0600

    finally working jax+cuda env!!

--- flake.nix
+++ flake.nix
@@ -1,66 +1,39 @@
 {
-  description = "An FHS shell for CUDA and Pixi.";
+  description = "A NixOS shell for using Latest Pixi";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
   outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
-
       pkgs = import nixpkgs {
         inherit system;
-        config.allowUnfree = true;
-        config.allowBroken = true;
-        config.nvidiaSupport = true;
+        config = {
+          allowUnfree = true;
+          allowBroken = true;
+          nvidiaSupport = true;
+        };
       };
 
       latest_pixi = pkgs.callPackage ./pixi.nix {
         inherit (pkgs) stdenv rustPlatform fetchFromGitHub pkg-config libgit2 openssl installShellFiles;
       };
-
       fhs = pkgs.buildFHSUserEnv {
         name = "cuda";
         targetPkgs = pkgs: with pkgs; [
           latest_pixi
-          linuxPackages.nvidia_x11_stable_open
-          libGLU
-          libGL
-          xorg.libICE
-          xorg.libSM
-          xorg.libX11
-          xorg.libXext
-          xorg.libXi
-          xorg.libXmu
-          xorg.libXrandr
-          xorg.libXrender
-          xorg.libXv
-          freeglut
-          zlib
-          ncurses5
-          stdenv.cc
-          binutils
-          ffmpeg
-          autoconf
-          curl
-          freeglut
-          gcc
+          linuxPackages.nvidia_x11
           git
-          gitRepo
+          gcc
           gnumake
           cmake
-          gnupg
-          gperf
-          libselinux
-          m4
-          procps
-          unzip
-          util-linux
-          wget
         ];
         profile = ''
           export CUDA_PATH=/home/jimh/DATA2/python/.pixi/envs/default
-          export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11_stable_open}/lib
-          export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11_stable_open}/lib"
+          export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib
+          export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
           export EXTRA_CCFLAGS="-I/usr/include"
           export KERAS_BACKEND="jax"
           export JAX_PLATFORM_NAME="cuda"
