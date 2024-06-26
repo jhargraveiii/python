08e2203e6c2818f4535dda1cddfb4d706f66c764
Jim Hargrave <jim.hargrave@strakergroup.com>
2024-06-26 12:18:06 -0600

    remove custom pixi

--- flake.nix
+++ flake.nix
@@ -2,10 +2,11 @@
   description = "A NixOS shell for using Latest Pixi";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs =
+    { self, nixpkgs, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -15,6 +16,10 @@
           nvidiaSupport = true;
           allowUnfree = true;
           nvidia.acceptLicense = true;
+          blasSupport = true;
+          blasProvider = pkgs.amd-blis;
+          lapackSupport = true;
+          lapackProvider = pkgs.amd-libflame;
         };
         hostPlatform = {
           gcc = {
@@ -34,30 +39,36 @@
         };
       };
 
-      latest_pixi = pkgs.callPackage ./pixi.nix {
-        inherit (pkgs) stdenv rustPlatform fetchFromGitHub pkg-config libgit2 openssl installShellFiles;
-      };
-
       nix = {
         settings = {
-          experimental-features = [ "nix-command" "flakes" ];
-          system-features = [ "benchmark" "big-parallel" "kvm" "nixos-test" "gccarch-znver3" ];
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+          system-features = [
+            "benchmark"
+            "big-parallel"
+            "kvm"
+            "nixos-test"
+            "gccarch-znver3"
+          ];
         };
       };
 
       fhs = pkgs.buildFHSUserEnv {
         name = "cuda";
-        targetPkgs = pkgs: with pkgs; [
-          latest_pixi
-          linuxPackages_latest.nvidia_x11
-          git
-          gcc
-          gfortran
-          gnumake
-          cmake
-          amd-blis
-          amd-libflame
-        ];
+        targetPkgs =
+          pkgs: with pkgs; [
+            pixi
+            linuxPackages_latest.nvidia_x11
+            git
+            gcc
+            gfortran
+            gnumake
+            cmake
+            amd-blis
+            amd-libflame
+          ];
         profile = ''
           export KITTY_PUBLIC_KEY=""
           export UV_HTTP_TIMEOUT=900
@@ -67,6 +78,7 @@
       };
     in
     {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
       devShells.${system}.default = fhs.env;
     };
 }
